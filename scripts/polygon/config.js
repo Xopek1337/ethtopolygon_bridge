183f356959a3b584cc762c89a16e9e624e99d612
Nikita <nakit21@gmail.com>
2022-04-17 23:35:30 +0300

    fix scripts

--- scripts/polygon/config.js
+++ scripts/polygon/config.js
@@ -22,8 +22,7 @@ module.exports = {
       erc721: process.env.POLYGON_ERC721,
     },
   },
-  SYNCER_URL: 'https://testnetv3-syncer.api.matic.network/api/v1', // Backend service which syncs the Matic sidechain state to a MySQL database which we use for faster querying. This comes in handy especially for constructing withdrawal proofs while exiting assets from Plasma.
-  WATCHER_URL: 'https://testnetv3-watcher.api.matic.network/api/v1', // Backend service which syncs the Matic Plasma contract events on Ethereum mainchain to a MySQL database which we use for faster querying. This comes in handy especially for listening to asset deposits on the Plasma contract.
+  WATCHER_URL: 'https://sentinel.matic.network/api/v2', // Backend service which syncs the Matic Plasma contract events on Ethereum mainchain to a MySQL database which we use for faster querying. This comes in handy especially for listening to asset deposits on the Plasma contract.
   user1: {
     // '<paste your private key here>' - A sample private key prefix with `0x`
     privateKey: process.env.PRIVATE_KEY,
